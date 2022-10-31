751b39e3299583d02bb1d6964b2f799506abd7c2
Borbola Anamaria <anamariaborbola@yahoo.com>
2022-10-31 17:58:42 +0200

    Laboratorul 3

--- Data/Anamaria_Borbola_Lab2Context.cs
+++ Data/Anamaria_Borbola_Lab2Context.cs
@@ -19,5 +19,7 @@ namespace Anamaria_Borbola_Lab2.Data
         public DbSet<Anamaria_Borbola_Lab2.Models.Publisher> Publisher { get; set; }
 
         public DbSet<Anamaria_Borbola_Lab2.Models.Author> Author { get; set; }
+
+        public DbSet<Anamaria_Borbola_Lab2.Models.Category> Category { get; set; }
     }
 }
--- Migrations/20221030214206_BookData.Designer.cs
+++ Migrations/20221030214206_BookData.Designer.cs
@@ -4,6 +4,7 @@ using Anamaria_Borbola_Lab2.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Anamaria_Borbola_Lab2.Migrations
 {
     [DbContext(typeof(Anamaria_Borbola_Lab2Context))]
-    partial class Anamaria_Borbola_Lab2ContextModelSnapshot : ModelSnapshot
+    [Migration("20221030214206_BookData")]
+    partial class BookData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
